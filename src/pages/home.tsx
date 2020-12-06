d185d06ff6a870b4a7a01865e00410b0caef62df
Armin Pfurtscheller <armin.pfurtscheller@cropster.org>
2020-12-06 11:57:35 +0100

    Add splashscreen

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {
   IonContent,
   IonHeader,
@@ -10,61 +10,64 @@ import {
   IonButtons,
   IonButton,
   IonIcon,
-  IonFooter,
-  IonSearchbar,
 } from "@ionic/react";
 
 import { Container, Section, Title } from "components";
 import messageIcon from "icons/message.svg";
 import avatarImg from "images/profile1.jpg";
 
+
 import { MyIdeas } from "./home/my-ideas";
 import { MyStream } from "./home/my-stream";
+import { SplashScreen } from "./home/splashscreen";
 
 import styles from "./home.module.css";
 
 export const Home: React.FC = () => (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar>
-        <IonItem slot="start" lines="none" className={styles.ionitem}>
-          <IonAvatar>
-            <img src={avatarImg} alt="" />
-          </IonAvatar>
-        </IonItem>
-        <IonButtons slot="end">
-          <IonButton color="dark">
-            <IonIcon slot="icon-only" src={messageIcon} />
-          </IonButton>
-        </IonButtons>
-        <IonTitle>
-          <div className="ion-text-center">
-            <Title noMargin={true}>vickies</Title>
-          </div>
-        </IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent fullscreen>
-      {/*<IonHeader collapse="condense">
+  <Fragment>
+    <SplashScreen />
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonItem slot="start" lines="none" className={styles.ionitem}>
+            <IonAvatar>
+              <img src={avatarImg} alt="" />
+            </IonAvatar>
+          </IonItem>
+          <IonButtons slot="end">
+            <IonButton color="dark">
+              <IonIcon slot="icon-only" src={messageIcon} />
+            </IonButton>
+          </IonButtons>
+          <IonTitle>
+            <div className="ion-text-center">
+              <Title noMargin={true}>vickies</Title>
+            </div>
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        {/*<IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>*/}
-      <Container>
-        <Section>
-          <div className={styles.myideas}>
-            <MyIdeas />
-          </div>
-        </Section>
-        <Section>
-          <MyStream />
-        </Section>
-      </Container>
-    </IonContent>
-    {/*<IonFooter>
+        <Container>
+          <Section>
+            <div className={styles.myideas}>
+              <MyIdeas />
+            </div>
+          </Section>
+          <Section>
+            <MyStream />
+          </Section>
+        </Container>
+      </IonContent>
+      {/*<IonFooter>
       <IonToolbar>
         <IonSearchbar value="XXX" />
       </IonToolbar>
     </IonFooter>*/}
-  </IonPage>
+    </IonPage>
+  </Fragment>
 );
---
+++ src/pages/home/splashscreen.tsx
@@ -0,0 +1,64 @@
+import React, { useMemo, useState, useCallback } from "react";
+import { createPortal } from "react-dom";
+
+import logoVariant1 from "icons/logo-variant1.svg";
+import logoVariant2 from "icons/logo-variant2.svg";
+import logoVariant3 from "icons/logo-variant3.svg";
+
+import { Text } from "components";
+
+import styles from "./splashscreen.module.css";
+
+const THEMES = [
+  {
+    svg: logoVariant1,
+    className: styles.variant1,
+  },
+  {
+    svg: logoVariant2,
+    className: styles.variant2,
+  },
+  {
+    svg: logoVariant3,
+    className: styles.variant3,
+  },
+];
+
+export const SplashScreen: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const variant = useMemo(() => THEMES[~~(Math.random() * THEMES.length)], []);
+  const clickHandler = useCallback(() => setIsVisible(false), []);
+  const wrapperClassNames = useMemo(
+    () =>
+      [
+        styles.wrapper,
+        variant.className,
+        isVisible ? styles.visible : styles.hidden,
+      ].join(" "),
+    [isVisible, variant]
+  );
+
+  return createPortal(
+    <div className={wrapperClassNames} onClick={clickHandler} tabIndex={-1}>
+      <div className={styles.content}>
+        <img
+          src={variant.svg}
+          className={styles.svg}
+          alt="Vickies - where ideas spark"
+        />
+        <div className={styles.text}>
+          <Text size="lg" bold={true}>
+            where
+          </Text>{" "}
+          <Text size="lg" type="special">
+            ideas
+          </Text>{" "}
+          <Text size="lg" bold={true}>
+            spark
+          </Text>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
+};
